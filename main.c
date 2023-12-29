4101ae5dcce0a5beb006d4848f01eb1da3dff5a1
Shiori Yonekura <syonekur@c1r21s5.42tokyo.jp>
2023-12-29 14:10:31 +0900

    fix error

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: syonekur <syonekur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 19:18:15 by syonekur          #+#    #+#             */
-/*   Updated: 2023/12/25 17:24:23 by syonekur         ###   ########.fr       */
+/*   Updated: 2023/12/29 14:09:21 by syonekur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ char	*cat_cmd_path(char *path, char *cmd)
 	cat_cmd_path = malloc(path_len + cmd_len + 2);
 	if (!cat_cmd_path)
 		return (NULL);
-	strlcpy(cat_cmd_path, path, path_len + 1);
+	ft_strlcpy(cat_cmd_path, path, path_len + 1);
 	cat_cmd_path[path_len] = '/';
-	strlcpy(cat_cmd_path + path_len + 1, cmd, cmd_len + 1);
+	ft_strlcpy(cat_cmd_path + path_len + 1, cmd, cmd_len + 1);
 	printf("%s\n", cat_cmd_path);
 	return (cat_cmd_path);
 }
@@ -47,12 +47,16 @@ void	free_memory(char **str)
 
 int	check_io_fd(int infile_fd, int outfile_fd)
 {
-	if (infile_fd == -1 || outfile_fd == -1)
+	if (infile_fd == -1 && outfile_fd == -1)
+		return (-1);
+	else if (infile_fd == -1)
 	{
-		if (infile_fd == -1)
-			close(outfile_fd);
-		if (outfile_fd == -1)
-			close(infile_fd);
+		close(outfile_fd);
+		return (-1);
+	}
+	else if (outfile_fd == -1)
+	{
+		close(infile_fd);
 		return (-1);
 	}
 	return (0);
@@ -70,9 +74,6 @@ int	main(int argc, char *argv[])
 	int	pipe_fd[2];
 	int	infile_fd;
 	int	outfile_fd;
-	int	status;
-	int	pid1;
-	int	pid2;
 
 	if (check_argc(argc) == -1)
 		return (-1);
@@ -83,9 +84,7 @@ int	main(int argc, char *argv[])
 	if (pipe(pipe_fd) == -1)
 		return (-1);
 	first_child_process(infile_fd, pipe_fd, argv);
-	waitpid(-1, &status, 0);
-	second_child_process(infile_fd, pipe_fd, argv);
-	waitpid(-1, &status, 0);
+	second_child_process(outfile_fd, pipe_fd, argv);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
 	return (0);
